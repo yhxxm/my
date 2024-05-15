6019111242bc89df1456e197c9d892d7e79e2a4c
yhxxm <109203732+yhxxm@users.noreply.github.com>
2024-05-15 16:09:02 +0800

    Add files via upload

---
+++ js/pptv66影视.js
@@ -0,0 +1,28 @@
+var rule={
+    title: 'pptv66影视',
+    host: 'https://pptv06.com',
+    url: '/vodshow/fyclass--------fypage---.html',
+    searchUrl: '/vodsearch/**----------fypage---.html',
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+    headers: {
+    'User-Agent': 'MOBILE_UA',
+    },
+    class_parse: '.navbar&&ul&&li;a&&Text;a&&href;/(\\d+).html',
+    class_exclude:'日更新|首页|APP|',
+    play_parse: true,
+    lazy: '',
+    limit: 6,
+    推荐: 'body&&.scroll-box;.module-poster-item.module-item;.module-poster-item-title&&Text;img&&data-original;.module-item-note&&Text;a&&href',
+    double: true,
+    一级: 'body&&.module-item;a&&title;img&&data-original;.module-item-note&&Text;a&&href',
+    二级: {
+    "title": "h1&&Text;.module-info-tag&&Text",
+    "img": ".lazyload&&data-original",
+    "desc": ".module-info-item:eq(1)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(3)&&Text",
+    "content": ".module-info-introduction&&Text",
+    "tabs": ".hisSwiper&&span",
+    "lists": ".module-play-list:eq(#id) a"
+    },
+    搜索: 'body .module-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href;.module-info-item-content&&Text',}
